965c9571d30e2f13444470b0d721227367a270cf
Merio <oncwnuNjCpyXmvkyUpjsiWW9knVQ@git.weixin.qq.com>
2020-06-15 00:25:13 +0800

    modifty connection

--- config/mysql_config.js
+++ config/mysql_config.js
@@ -4,8 +4,7 @@ const mysqlConfig= {
     user: "merio_db",
     password: "Merio2020",
     database: "merio_db",
-  
-    host:"http://172.18.164.126",
+    host: "rm-2zed76hhd8hls6x68125010.mysql.rds.aliyuncs.com",
     port: 3306
 }
 
