cbe59a3b9863ff3a94fd3e112d708c0ae8528d1d
MrMeno <myzonemin@qq.com>
2020-11-20 00:50:09 +0800

    服务端部署配置

--- start_server.sh
+++ start_server.sh
@@ -6,31 +6,31 @@ export KOA_HOME
 echo "开始编译typescript文件"
 TSC_COMPILE=`tsc`
 sleep 1
-if [$TSC_COMPILE -eq 0];
-sleep 1
+if ($TSC_COMPILE -eq 0);
 then
+    sleep 1
     echo "文件编译完成 开始重构"
     file_move1=`cp package.json ./dist`
     sleep 1
     file_move2=`cp -a engine ./dist/`
     sleep 1
     file_move3=`cp -a statics ./dist/`
-    if [[ ($file_move3 -eq 0) && ($file_move1 -eq 0) && ($file_move2 -eq 0)]];
+    if ($file_move3 -eq 0);
     then
         RE=`cd dist`
         sleep 1
-        if [[ $file_move -eq 0 ]];
+        if ($file_move -eq 0);
         then
             echo "重构完成"
             echo "koa-server构建完成启动"
-            file_move4=`cp -a ${KOA_HOME}+/dist ${SERVER_HOME}`
+            file_move4=`cp -a ${KOA_HOME}/dist ${SERVER_HOME}`
             sleep 1
-            if [[ $file_move4 -eq 0 ]];
+            if ($file_move4 -eq 0);
             then
                 TO_SERVER=`cd ${SERVER_HOME}`
                 sleep 1
                 SERVER_INIT=`cnpm i`
-                if [[ $SERVER_INIT -eq 0 ]];
+                if ($SERVER_INIT -eq 0);
                 then
                     START_SERVER=`npm run prd:server`
                 fi
