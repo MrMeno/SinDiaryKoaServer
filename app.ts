b80b725d7d3b77b7de4ae38d8d316f613474dba4
MrMeno <myzonemin@qq.com>
2020-11-25 22:11:45 +0800

    配置变更

--- app.ts
+++ app.ts
@@ -5,11 +5,13 @@ import json from 'koa-json';
 const onError = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const KoaStatic = require('koa-static');
+const cors=require("koa2-cors");
 import koaBody from 'koa-body';
 import KoaLogger from 'koa-logger';
 import addRouter from './utils/define/routeHook'
 import {jsonHandler} from './utils/middleware/json'
 import {errorHandler} from './utils/middleware/error'
+ 
 import path from 'path'
 const app=new Koa();
 const router=new koaRouter();
@@ -25,11 +27,19 @@ app.use(json())
 app.use(koaBody({
   multipart: true, 
 }))
+app.use(cors({
+  maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+  credentials: true, //是否允许发送Cookie
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
+}))
 app.use(KoaLogger())
 app.use(KoaStatic(path.join(__dirname, 'statics')))
 app.use(views(__dirname + '/engine', {
   extension: 'pug'
 }))
+ 
 app.use(async(ctx:Context, next:any) => {
   const start = new Date()
   await next()
